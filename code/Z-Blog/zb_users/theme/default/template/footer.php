7ce6b76c11032214913403df0a3d50cc7dfa92cc
serverless-devs <serverless-devs@alibaba-inc.com>
2022-03-17 03:20:28 +0000

    Initial commit

---
+++ code/Z-Blog/zb_users/theme/default/template/footer.php
@@ -0,0 +1,8 @@
+{* Template Name:公共底部 *}
+<div id="divBottom">
+  <p id="BlogPowerBy">Powered By {$zblogphphtml}</p>
+  <p id="BlogCopyRight">{$copyright}</p>
+</div>
+<div class="clear"></div>
+
+{$footer}
